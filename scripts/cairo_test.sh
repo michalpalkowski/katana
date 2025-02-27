f8aee0fb1e71094eef01930de531d9643b5b811b
Ammar Arif <evergreenkary@gmail.com>
2025-02-27 13:26:41 -0500

    chore: remove unused scripts

--- scripts/cairo_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-cargo run -r --bin sozo -- --manifest-path crates/dojo/core/Scarb.toml test $@
-cargo run -r --bin sozo -- --manifest-path examples/spawn-and-move/Scarb.toml test $@
